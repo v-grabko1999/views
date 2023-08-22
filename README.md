1251b27b7472724e55b7c4f5aa8da3927fb7975d
Volodymyr Grabko <vgrabkowot@gmail.com>
2023-08-22 22:45:16 +0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # views
+
+```Go
+
+    v := views.New(views.Config{
+		Dir:        "./examples",
+		Extensions: []string{".tmpl"},
+		Compress:   true,
+		Dev:        false,
+	})
+
+	v.Func(template.FuncMap{
+		"tolower": strings.ToLower,
+	})
+
+	if err := v.Load(); err != nil {
+		log.Fatalln("views load", err)
+	}
+
+	if err := v.Execute(os.Stdout, "child.tmpl", nil); err != nil {
+		log.Fatalln("views Execute", err)
+	}
+
+
+```
+
+```code
+PASS
+coverage: 83.8% of statements
+ok      views   0.279s
+```
--- views_test.go
+++ views_test.go
@@ -4,21 +4,29 @@ import (
 	"html/template"
 	"os"
 	"strings"
+	"testing"
 	"views"
 )
 
-func ExampleNew() {
-	xt := views.New(views.Config{
+func TestNew(t *testing.T) {
+	v := views.New(views.Config{
 		Dir:        "./examples",
 		Extensions: []string{".tmpl"},
+		Compress:   true,
+		Dev:        false,
 	})
 
-	xt.Func(template.FuncMap{
+	v.Func(template.FuncMap{
 		"tolower": strings.ToLower,
 	})
-	_ = xt.Load()
 
-	_ = xt.Execute(os.Stdout, "child.tmpl", nil)
-	/* Output: Hello from child.tmpl
-	Hello from partials/question.tmpl */
+	if err := v.Load(); err != nil {
+		t.Fatal("views load", err)
+	}
+
+	if err := v.Execute(os.Stdout, "child.tmpl", nil); err != nil {
+		t.Fatal("views Execute", err)
+	}
+
+	t.Log("views OK")
 }
